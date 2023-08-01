de1714d8b5ddb7016014334b98ed7a789028c1c0
hyunho4621 <137243882+hyunho4621@users.noreply.github.com>
2023-08-01 21:41:39 +0900

    2023-08-01 JS 변수

---
+++ chapter04/variable.js
@@ -0,0 +1,28 @@
+// 기본적인 변수 사용 방법
+var result = 10 + 20;
+
+console.log(result);
+
+
+// var 키워드
+var score;
+
+console.log(score);
+
+// 값의 할당 - 1
+var score;
+score = 80;
+console.log(score);
+
+// 값의 할당 - 2
+console.log(score); // undefined
+
+var score = 80;
+
+console.log(score);
+
+// 아래 예제의 실행 결과는 무엇일까?
+score = 80;
+var score;
+
+console.log(score);
